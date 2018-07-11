d98c5ed42abe4d3fff07674ed3201fd438ba0389
Jonathan Glanz <jonathan.glanz@gmail.com>
2018-07-11 01:44:50 -0700

    Android FFMPEG adjustments for ABIs

--- ffmpeg/scripts/configure.sh
+++ ffmpeg/scripts/configure.sh
@@ -22,8 +22,7 @@ ARCH=
 HOST_OS=$(tolower $(uname -s))
 TARGET_OS=${HOST_OS}
 ANDROID=0
-
-env
+ANDROID_ARCH=
 
 if [ "${CMAKE_SYSROOT}" != "" ]; then
     CROSS_COMPILING=1
@@ -33,12 +32,22 @@ if [ "${CMAKE_SYSROOT}" != "" ]; then
     CROSS_SUFFIX=${CMAKE_CROSS_SUFFIX}
     TARGET_OS=$(tolower ${CMAKE_SYSTEM_NAME})
     
-    # ${CMAKE_SYSTEM_PROCESSOR}
-    
     if [ "${TARGET_OS}" = "android" ]; then
-        ARCH=aarch64
-        # TARGET_OS=linux
         ANDROID=1
+        ARCH=${ANDROID_ABI}
+        ANDROID_ARCH=${ARCH}
+        case "${ARCH}" in
+            arm64-v8a) 
+                ARCH=aarch64
+                ANDROID_ARCH=arm64
+                ;;
+            armeabi-v7a)
+                ARM=arm
+                ANDROID_ARCH=arm 
+                ;;
+        esac
+
+        
         CC=${ANDROID_C_COMPILER}
         CXX=${ANDROID_CXX_COMPILER}
         if [ "${ANDROID_TOOLCHAIN_PREFIX}" != "" ]; then
@@ -61,10 +70,7 @@ OPTS="--incdir=${CXXPODS_BUILD_INCLUDE} \
     --disable-ffserver \
     --disable-debug \
     --enable-decoder=h264 \
-    --enable-decoder=h264_mediacodec \
     --enable-decoder=hevc \
-    --enable-hwaccel=hevc_mediacodec \
-    --enable-decoder=hevc_mediacodec \
     --enable-parser=h264 \
     --enable-pthreads \
     --enable-hardcoded-tables \
@@ -90,26 +96,28 @@ if [ "${CROSS_COMPILING}" = "1" ]; then
      --ar=${CROSS_PREFIX}ar \
      --as=${CROSS_PREFIX}gcc \
      --extra-libs=-lgcc"
-    #   \
-    #  --as=${CROSS_PREFIX}gcc"
-    #-isystem ${CMAKE_SYSROOT}/usr/include/${ANDROID_HEADER_TRIPLE}
-
-    ANDROID_PLATFORM_ROOT=${ANDROID_NDK}/platforms/android-27/arch-arm64
+  
+    
     
     echo "SYSROOT=${CMAKE_SYSROOT}"
-    CFLAGS=" ${CFLAGS} -I${ANDROID_PLATFORM_ROOT}/ -I${CMAKE_SYSROOT}/usr/include -I${CMAKE_SYSROOT}/usr/include/${ANDROID_HEADER_TRIPLE} -I${ANDROID_PLATFORM_ROOT}/include -DANDROID -Dipv6mr_interface=ipv6mr_ifindex -fasm -Wno-psabi -fno-short-enums"
-    EXTRA_LD="-L${ANDROID_PLATFORM_ROOT}/usr/lib64"
-    LDFLAGS="${LDFLAGS} -Wl,-rpath-link=${ANDROID_PLATFORM_ROOT}/usr/lib ${EXTRA_LD} -L${ANDROID_PLATFORM_ROOT}/usr/lib  -L${CXXPODS_BUILD_LIB} -nostdlib -lm -lc -ldl"
-
+    
     if [ "${ANDROID}" == "1" ]; then
+        ANDROID_PLATFORM_ROOT=${ANDROID_NDK}/platforms/android-${ANDROID_NATIVE_API_LEVEL:-27}/arch-${ANDROID_ARCH}
         ANDROID_PLATFORM_PATH=${ANDROID_SYSTEM_LIBRARY_PATH}
+
+        CFLAGS=" ${CFLAGS} -I${ANDROID_PLATFORM_ROOT}/ -I${CMAKE_SYSROOT}/usr/include -I${CMAKE_SYSROOT}/usr/include/${ANDROID_HEADER_TRIPLE} -I${ANDROID_PLATFORM_ROOT}/include -DANDROID -Dipv6mr_interface=ipv6mr_ifindex -fasm -Wno-psabi -fno-short-enums"
+        EXTRA_LD="-L${ANDROID_PLATFORM_ROOT}/usr/lib64"
+        LDFLAGS="${LDFLAGS} -Wl,-rpath-link=${ANDROID_PLATFORM_ROOT}/usr/lib ${EXTRA_LD} -L${ANDROID_PLATFORM_ROOT}/usr/lib  -L${CXXPODS_BUILD_LIB} -nostdlib -lm -lc -ldl"
         
         echo "Platform path: ${ANDROID_PLATFORM_PATH}"
-        #-L${ANDROID_PLATFORM_PATH}/usr/lib64
-        # -L${ANDROID_PLATFORM_PATH}/usr/lib
+        
         LDFLAGS=" ${LDFLAGS} -llog"
-        OPTS="${OPTS} --enable-jni --enable-mediacodec"
+        OPTS="${OPTS} --enable-jni \
+            --enable-mediacodec \
+            --enable-decoder=h264_mediacodec \
+            --enable-hwaccel=hevc_mediacodec \
+            --enable-decoder=hevc_mediacodec "
     fi
 fi
 
-./configure ${OPTS} --extra-cflags="${CFLAGS}" --extra-cxxflags="${CFLAGS}" --extra-ldflags="${LDFLAGS}"
+./configure ${OPTS} ${FFMPEG_OPTS} --extra-cflags="${CFLAGS}" --extra-cxxflags="${CFLAGS}" --extra-ldflags="${LDFLAGS}"
